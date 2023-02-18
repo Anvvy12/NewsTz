3cfbce36566703477d52f8adaa26b7dfb5317c22
Bogdan <youbody7@gmail.com>
2023-02-18 18:01:42 +0200

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Footer from './footer/Footer';
+import Header from './header/Header';
+import Main from './main/Main';
 
 const App = () => {
-  return <div>AAAA</div>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
