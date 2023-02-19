8dd197a8d34705aa891cd02a25dab72dd1dc6eb8
Bogdan <youbody7@gmail.com>
2023-02-19 18:42:46 +0200

    s

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import './index.scss';
 
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className="footer">
+      <ul className="footer-list">
+        <li className="footer-list__item">
+          <a href="https://github.com/Anvvy12" target="_blank">
+            My GitHub
+          </a>
+        </li>
+        <li className="footer-list__item">
+          <a href="https://www.linkedin.com/in/bogdan-roman-b498b924b/" target="_blank">
+            My LinkedIn
+          </a>
+        </li>
+        <li className="footer-list__item">
+          <a
+            href="https://docs.google.com/document/d/10WwL4OJSFBBRmoIpCZeAW87iNYES-VuJKHpV4Fd45vk/edit"
+            target="_blank"
+          >
+            My CV
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
